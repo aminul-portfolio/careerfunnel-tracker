ded72799afd5402ac6af9af63db53954f9f4b594
Aminul Islam <sumon_islam2009@yahoo.com>
2026-05-17 13:14:32 +0100

    Sprint 21A.2: add sidebar active state

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,24 @@
-console.log("CareerFunnel Tracker loaded.");
+(function () {
+    var path = window.location.pathname;
+    var links = document.querySelectorAll(".sidebar-link");
+    var bestMatch = null;
+    var bestLength = -1;
+
+    links.forEach(function (link) {
+        var href = link.getAttribute("href");
+        if (!href) {
+            return;
+        }
+
+        if (path === href || (href !== "/" && path.startsWith(href))) {
+            if (href.length > bestLength) {
+                bestMatch = link;
+                bestLength = href.length;
+            }
+        }
+    });
+
+    if (bestMatch) {
+        bestMatch.classList.add("active");
+    }
+})();
